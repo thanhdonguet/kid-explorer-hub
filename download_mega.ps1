384035bf5c40c984739d3aa14a0311c63645fd3f
thanhdonguet <thanhdonguet@users.noreply.github.com>
2026-07-14 15:47:23 +0700

    feat: enlarge UI for kids, expand vocab database to 260 words and svgs

---
+++ download_mega.ps1
@@ -0,0 +1,46 @@
+$downloads = @{
+  'apple' = '1f34e'; 'ant' = '1f41c'; 'alien' = '1f47d'; 'airplane' = '2708'; 'ambulance' = '1f691'; 'avocado' = '1f951'; 'anchor' = '2693'; 'axe' = '1fa93'; 'alarm' = '23f0'; 'art' = '1f3a8';
+  'bear' = '1f43b'; 'balloon' = '1f388'; 'banana' = '1f34c'; 'bell' = '1f514'; 'bird' = '1f426'; 'boat' = '26f5'; 'book' = '1f4d6'; 'bus' = '1f68c'; 'bread' = '1f35e'; 'butterfly' = '1f98b';
+  'cat' = '1f431'; 'car' = '1f697'; 'carrot' = '1f955'; 'castle' = '1f3f0'; 'cow' = '1f42e'; 'camel' = '1f42b'; 'cake' = '1f382'; 'camera' = '1f4f7'; 'candy' = '1f36c'; 'clock' = '1f550';
+  'dog' = '1f436'; 'duck' = '1f986'; 'dolphin' = '1f42c'; 'dinosaur' = '1f996'; 'dress' = '1f457'; 'drum' = '1f941'; 'door' = '1f6aa'; 'diamond' = '1f48e'; 'donut' = '1f369'; 'deer' = '1f98c';
+  'elephant' = '1f418'; 'egg' = '1f95a'; 'eye' = '1f441'; 'ear' = '1f442'; 'earth' = '1f30d'; 'eagle' = '1f985'; 'envelope' = '2709'; 'engine' = '1f682'; 'eggplant' = '1f346'; 'elf' = '1f9dd';
+  'fox' = '1f98a'; 'fish' = '1f41f'; 'frog' = '1f438'; 'flower' = '1f338'; 'fire' = '1f525'; 'flag' = '1f3c1'; 'foot' = '1f9b6'; 'fairy' = '1f9da'; 'feather' = '1faeb'; 'fries' = '1f35f';
+  'giraffe' = '1f992'; 'ghost' = '1f47b'; 'guitar' = '1f3b8'; 'gorilla' = '1f98d'; 'goat' = '1f410'; 'gift' = '1f381'; 'glasses' = '1f453'; 'globe' = '1f30e'; 'grapes' = '1f347'; 'garlic' = '1f9c4';
+  'horse' = '1f434'; 'house' = '1f3e0'; 'hat' = '1f3a9'; 'heart' = '2764'; 'helicopter' = '1f681'; 'hamburger' = '1f354'; 'honey' = '1f36f'; 'hedgehog' = '1f994'; 'hamster' = '1f439'; 'hippo' = '1f9db';
+  'ice_cream' = '1f366'; 'ice' = '1f9ca'; 'igloo' = '1f3e8'; 'island' = '1f3dd'; 'insect' = '1f41b'; 'ink' = '1f58b'; 'instrument' = '1f3ba'; 'iris' = '1f441'; 'idea' = '1f4a1'; 'iguana' = '1f9ce';
+  'juice' = '1f9c3'; 'jeans' = '1f456'; 'jacket' = '1f9e5'; 'jaguar' = '1f406'; 'joker' = '1f0cf'; 'joystick' = '1f579'; 'jug' = '1f3fa'; 'jelly' = '1f36e'; 'jewel' = '1f48e'; 'jet' = '2708';
+  'kangaroo' = '1f998'; 'key' = '1f511'; 'kite' = '1fa81'; 'keyboard' = '2328'; 'kiwi' = '1f95d'; 'koala' = '1f428'; 'knife' = '1f52a'; 'knot' = '1fada'; 'king' = '1f934'; 'kiss' = '1f48b';
+  'lion' = '1f981'; 'leaf' = '1f343'; 'lemon' = '1f34b'; 'lock' = '1f512'; 'laptop' = '1f4bb'; 'ladybug' = '1f41e'; 'lizard' = '1f9ce'; 'llama' = '1f999'; 'lobster' = '1f99e'; 'lollipop' = '1f36d';
+  'monkey' = '1f435'; 'mouse' = '1f42d'; 'moon' = '1f319'; 'magnet' = '1f9f2'; 'map' = '1f5fa'; 'mushroom' = '1f344'; 'melon' = '1f348'; 'microscope' = '1f52c'; 'motorbike' = '1f3cd'; 'milk' = '1f95b';
+  'nest' = '1fab9'; 'nose' = '1f443'; 'nut' = '1f95c'; 'ninja' = '1f977'; 'newspaper' = '1f4f0'; 'note' = '1f3b5'; 'necklace' = '1f4ff'; 'nail' = '1f485'; 'noodle' = '1f35c'; 'night' = '1f303';
+  'owl' = '1f989'; 'octopus' = '1f419'; 'orange' = '1f34a'; 'onion' = '1f9c5'; 'ocean' = '1f30a'; 'ox' = '1f402'; 'otter' = '1f9a6'; 'ostrich' = '1f426'; 'olive' = '1fad2'; 'oyster' = '1f9aa';
+  'pig' = '1f437'; 'penguin' = '1f427'; 'panda' = '1f43c'; 'pizza' = '1f355'; 'pear' = '1f350'; 'peach' = '1f351'; 'parrot' = '1f99c'; 'piano' = '1f3b9'; 'pencil' = '270f'; 'pineapple' = '1f34d';
+  'queen' = '1f478'; 'quilt' = '1f6cf'; 'quarter' = '1fa99'; 'question' = '2753'; 'quill' = '1f58b';
+  'rabbit' = '1f430'; 'rocket' = '1f680'; 'robot' = '1f916'; 'rose' = '1f339'; 'ring' = '1f48d'; 'rainbow' = '1f308'; 'rat' = '1f400'; 'rooster' = '1f413'; 'rhino' = '1f98f'; 'rice' = '1f35a';
+  'snake' = '1f40d'; 'star' = '2b50'; 'sun' = '2600'; 'spider' = '1f577'; 'snail' = '1f40c'; 'shark' = '1f988'; 'sheep' = '1f411'; 'sandwich' = '1f354'; 'shoe' = '1f45e'; 'scissors' = '2702';
+  'tiger' = '1f42f'; 'turtle' = '1f422'; 'tree' = '1f333'; 'train' = '1f682'; 'truck' = '1f69a'; 'tractor' = '1f69c'; 'tomato' = '1f345'; 'taco' = '1f32e'; 'tooth' = '1f9b7'; 'tent' = '26fa';
+  'umbrella' = '2602'; 'unicorn' = '1f984'; 'ufo' = '1f6f8'; 'uniform' = '1f9ec'; 'unlock' = '1f513'; 'up' = '2b06';
+  'violin' = '1f3bb'; 'volcano' = '1f30b'; 'vampire' = '1f9db'; 'van' = '1f690'; 'vest' = '1f9ba'; 'video' = '1f4f9'; 'virus' = '1f9a0'; 'vase' = '1f3fa';
+  'whale' = '1f433'; 'wolf' = '1f43a'; 'worm' = '1f41b'; 'watermelon' = '1f349'; 'watch' = '231a'; 'window' = '1fa9f'; 'wheel' = '1f6de'; 'wood' = '1fab5'; 'web' = '1f578'; 'waffle' = '1f9c7';
+  'x-ray' = '1fa7b';
+  'yak' = '1f402'; 'yarn' = '1f9f6'; 'yo-yo' = '1fa80'; 'yacht' = '1f6a4';
+  'zebra' = '1f993'; 'zombie' = '1f9df'; 'zipper' = '1f9e0'; 'zero' = '0030'
+}
+
+# Add default SSL/TLS to avoid issues
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+foreach ($key in $downloads.Keys) {
+  $hex = $downloads[$key]
+  $url = "https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/svg/$hex.svg"
+  $dest = "www\img\vocab\$key.svg"
+  if (-Not (Test-Path $dest)) {
+    try {
+        Invoke-WebRequest -Uri $url -OutFile $dest -UseBasicParsing
+        Write-Host "Downloaded $dest"
+    } catch {
+        Write-Host "Failed to download $url"
+    }
+  }
+}
+Write-Host "All mega downloads complete."
